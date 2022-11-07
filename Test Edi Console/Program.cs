54a5f49cc5634cc0bfcb01983d7702b2e1e5a04b
olah.istvan <Olah.Istvan@compuworx.hu>
2022-11-07 14:11:07 +0100

    learning

---
+++ Test Edi Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Test_Edi_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test Edi Net6/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+//
+using indice.Edi;
+using indice.Edi.Tests.Models;
+
+Console.WriteLine("Hello, World!");
+//var grammar = EdiGrammar.NewTradacoms();
+//var interchange = default(Interchange);
+//using (var stream = new StreamReader(@"c:\temp\sample.edi")) {
+//    interchange = new/* indice.Edi.*/ EdiSerializer().Deserialize<Interchange>(stream, grammar);
+//}
